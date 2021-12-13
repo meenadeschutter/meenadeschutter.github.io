8776c47c97a359fef02f4f3d29a57f0896589369
Meena De Schutter <meenadeschutter@gmail.com>
2021-12-13 14:47:29 -0800

    Reformat layout

---
+++ resume/loader.js
@@ -0,0 +1,21 @@
+const preloader = document.getElementById('preloader');
+
+const fadeEffect =
+    setInterval(() => {
+        // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
+        if (!preloader.style.opacity) {
+            preloader.style.opacity = 1;
+        }
+        setTimeout(function() {
+            if (preloader.style.opacity > 0) {
+                preloader.style.opacity -= 0.3;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 2000);
+    }, 100);
+
+window.addEventListener('load', () => {
+    fadeEffect;
+    setTimeout(() => { preloader.remove(); }, 2500)
+});
